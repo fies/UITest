25d689b66d1099bb7f531f67b5d61c690b8f7c2f
fies <esfikries@gmail.com>
2020-04-25 16:53:59 +0300

    version 3 eklendi

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -2,6 +2,7 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("version 1");
         System.out.println("version 2");
+		System.out.println("version 3");
         System.out.println("version 4");
     }
 }
